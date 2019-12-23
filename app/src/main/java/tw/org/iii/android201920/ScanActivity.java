b5038c237820d9448f3d93153dc0153eea68cbad
brad <brad@brad.tw>
2019-12-23 18:58:47 +0800

    4

--- app/src/main/java/tw/org/iii/android201920/ScanActivity.java
+++ app/src/main/java/tw/org/iii/android201920/ScanActivity.java
@@ -3,12 +3,39 @@ package tw.org.iii.android201920;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class ScanActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanActivity extends AppCompatActivity
+        implements ZXingScannerView.ResultHandler{
+    private ZXingScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+        mScannerView.setAspectTolerance(0.5f);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+    @Override
+    public void handleResult(Result rawResult) {
+        Log.v("brad", rawResult.getText());
+        finish();
     }
 }
